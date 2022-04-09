a77a91538a13ac026eb7284326c0a42e91ed68ee
RJVB09 <49564898+RJVB09@users.noreply.github.com>
2022-04-09 13:24:00 +0200

    added comments

--- MainScript.py
+++ MainScript.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python
+
+#import de nodige libraries
 from gps import *
 from requests import get
 import RPi.GPIO as GPIO
@@ -9,31 +11,30 @@ import os
 import subprocess
 import urllib
 
-#PINS
+#pin voor de valve.
 SOLENOID_PIN = 4
 
-#GPS SETUP
-#os.system("sudo gpsd /dev/serial0 -F /var/run/gpsd.sock")
-#subprocess.run(["sudo", "gpsd", "/dev/serial0", "-F", "/var/run/gpsd.sock"])
+#maak nieuwe gps class.
 gpsd = gps(mode=WATCH_ENABLE|WATCH_NEWSTYLE)
 
-#MiscVariables
-maximum = 570 #570 # Maximum value at full humidity
-setHumidity = 30 # Wanted humidity
-interval = 56 # Interval in s between moisturizations
+#variabelen.
+maximum = 570 #570 #Rauwe input van sensor voor max waarde.
+setHumidity = 30 #gewenste grondvochtigheid.
+interval = 56 #tijd tussen elke iteratie.
 apiKey = '4a8f8a6f70312feeb3e1303b63615f42'
-debug = True
+debug = False #voor debuggen.
 
 #GPIO SETUP
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(SOLENOID_PIN,GPIO.OUT)
 #GPIO.output(SOLENOID_PIN,GPIO.HIGH)
 
+#laatst geldige coordinaten aan het begin naar 0,0 zetten.
 lastValidLon = 0.0
 lastValidLat = 0.0
 
-def GPS():
-        report = gpsd.next() #
+def GPS(): #method voor GPS data verkrijgen.
+        report = gpsd.next()
         
         lon = 0.0
         lat = 0.0
@@ -42,11 +43,8 @@ def GPS():
         global lastValidLon
         
         if report['class'] == 'TPV':
-            lat = getattr(report,'lat',0.0)
+            lat = getattr(report,'lat',0.0) #report aflezen.
             lon = getattr(report,'lon',0.0)
-            
-            #print(getattr(report,'lat',0.0),"N \t")
-            #print(getattr(report,'lon',0.0),"E \t")
         if (lat == 0.0):
             lat = lastValidLat
         else:
@@ -57,12 +55,9 @@ def GPS():
         else:
             lastValidLon = lon
             
-        #print(lastValidLat),
-        #print(lastValidLon)
-        
         return lon, lat
 
-class MCP3008:
+class MCP3008: #class voor het aflezen van de analoog naar digitaal chip.
     def __init__(self, chip_select):
         self.spi = spidev.SpiDev()
         self.spi.open(0, chip_select)
@@ -74,38 +69,35 @@ class MCP3008:
         return data
 
 class weather:
-    def call(self):
-        #call weather
+    def call(self): #method voor het aflezen van de API met de huidige coordinaten.
+        #GPS() oproepen
         lon, lat = GPS()
-        #print(lon)
-        #print(lat)
+        
+		
         url = 'https://api.openweathermap.org/data/2.5/onecall?lat=' + str(lat) + '&lon=' + str(lon) + '&exclude=minutely,daily&appid=' + apiKey
+		#                                                                        ^ url opbouw
         
-        #check for connection, otherwise return POP = 0.0
+        #check voor internet
         connection = True
-        
-        #try: urllib.request.urlopen(url)
-        #except urllib.error.URLError:
-        #    print("no connection")
-        #    connection = False
         try:
             urllib.urlopen(url)
         except:
             connection = False
-        
+			
+		
         if (connection and lon != 0.0 and lat != 0.0):
             jsonString = json.dumps(get(url).json())
             jsonDictionary = json.loads(jsonString)
-            return float(jsonDictionary['hourly'][int(1)]['pop']) #Probability of precipitation
+            return float(jsonDictionary['hourly'][int(1)]['pop'])
         else:
-            return 0.0
+            return 0.0 #voor het geval er geen internet is wordt er verwacht dat het droog is.
         
 
-mcp = MCP3008(0)
+mcp = MCP3008(0) #nieuwe MCP class om af te lezen.
 w = weather()
 
 try:
-    while True:
+    while True: #Main loop die zich om de zoveel tijd herhaalt
         time.sleep(interval) 
         val = (((1023 - mcp.analogInput(0))/570)*100) #MOISTURE SENSOR (570) max humidity
         #val = (1023 - mcp.analogInput(0) - 480)/5.12 #POTMETER
@@ -118,7 +110,7 @@ try:
             GPIO.output(SOLENOID_PIN,GPIO.LOW)
             addWater = True
             
-        if (debug):
+        if (debug): #voor debuggen.
             print("------DEBUG------")
             #print("pop: ",weather.call())
             lon, lat = GPS()
@@ -127,5 +119,5 @@ try:
             print("addWater: ",addWater)
             print("-----------------")
         
-except (KeyboardInterrupt, SystemExit):
+except (KeyboardInterrupt, SystemExit): #waneer het programma stop moeten alle pins gecleared worden
     GPIO.cleanup()
